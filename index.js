f60c74677b1e27ebd897bdfda8926fd96ec552c7
Limbani <lchaponda@umass.edu>
2024-12-23 22:51:51 +0400

    home.html -> index.html rename

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+//onclick handlers for section cards to other pages
+
+document.getElementById("Projects").addEventListener("click", () => { 
+    window.location.href = "pages/projects/projects.html";
+}); 
